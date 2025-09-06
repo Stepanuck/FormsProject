28bf6ae8a8e21f853be03f4c3fec33bff2745a20
Stepanuck <marinozzitomas@hotmail.com>
2025-09-05 23:28:38 -0300

    Agregado de atributos
    Le agregue atributos a la clase Articulo en la carpeta dominio

--- winformProjectNegocio/dominio/Articulo.cs
+++ winformProjectNegocio/dominio/Articulo.cs
@@ -8,5 +8,22 @@ namespace dominio
 {
     public class Articulo
     {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public List<Imagen> Imagenes { get; set; } = new List<Imagen>();
+        public decimal Precio { get; set; }
+        public Articulo()
+        {
+            Marca = new Marca();
+            Categoria = new Categoria();
+        }
+        public override string ToString()
+        {
+            return Nombre;
+        }
     }
 }
